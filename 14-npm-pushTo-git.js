5272bf74ecf57b09f1b22c8a6db538471b6c9baf
Milind Bhatt <milind.bhatt@sap.com>
2023-03-16 22:13:31 +0100

    dummy1

--- 14-npm-pushTo-git.js
+++ 14-npm-pushTo-git.js
@@ -3,3 +3,4 @@
 // git branch -M main
 // git push -u origin main
 // https://github.com/milind-nerd/temp-node-nut.git
+// tutorial : https://www.jcchouinard.com/create-your-first-github-project-in-vscode/#:~:text=Create%20a%20Github%20Repository%20From%20VSCode,-VSCode%20has%20the&text=Press%20Ctrl%20%2B%20Shift%20%2B%20P%20(,Publish%20and%20push%20the%20changes.
